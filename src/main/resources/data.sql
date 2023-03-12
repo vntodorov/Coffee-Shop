14dc2b9347c58e363cf28b719eff26a67449386d
vntodorov <vntodorov02@gmail.com>
2023-03-12 19:53:49 +0200

    Front-end changes - changing the index page!

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,9 @@
 INSERT INTO brands(description, image_url, name)
 VALUES ("Good brand!", "https://www.nestle.com/sites/default/files/styles/brand_logo/public/nescafe-logo-square.png?h=a7e6d17b&itok=HbeWWpwO", "Nescafe"),
  ("Amazing brand!", "https://play-lh.googleusercontent.com/zScFS2Y-H_crW7WbEXz4GbWGv6cuNOg7bWRFs0L19936utOhpDpoEc4qSG6jYNdUYnc", "Tim Hortons");
+
+INSERT INTO users(created, email, first_name, image_url, last_name, middle_name, password, username)
+VALUES ('2023-03-12', 'admin@abv.bg', 'admin', null, 'admin', 'admin', '5fb750006d3cd45f1c35a8361accfae5ebdae36f76799c756c6822ab2feeb41652471508140ff90e', 'admin');
+
+--INSERT INTO users_roles(user_id, role_id)
+--VALUES (1,1),(1,2);
